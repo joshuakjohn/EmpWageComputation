619708d96a93eeaf9f46e1fc81293c60db15ef89
Joshua K John <joshuakjohn7@gmail.com>
2024-09-11 00:58:14 +0530

    Modified the code to work using switch case

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -4,25 +4,38 @@ public class EmpWageComputation {
         System.out.println("Welcome to Employee Wage Computation Program");
 
         // UC 1 - Check Employee is Present or Absent
-        int isPresent = (int)(Math.random() * 2); // Generates 0 or 1
+        int isPresent = (int) (Math.random() * 2); // Generates 0 or 1
 
         if (isPresent == 1) {
             System.out.println("Employee is Present");
 
-            // UC 3 - Add Part-time Employee & Wage
+            // UC 3 - Add Part-time Employee & Wage using Switch Case
             int wagePerHour = 20;
             int fullDayHour = 8;
             int partTimeHour = 4;
-            int employeeType = (int)(Math.random() * 2); // Generates 0 or 1
+            int employeeType = (int) (Math.random() * 2); // Generates 0 or 1
 
             int dailyWage = 0;
-            if (employeeType == 1) {
-                dailyWage = wagePerHour * fullDayHour;
-                System.out.println("Full-Time Employee Wage: " + dailyWage);
-            } else {
-                dailyWage = wagePerHour * partTimeHour;
-                System.out.println("Part-Time Employee Wage: " + dailyWage);
+            int workHours = 0;
+
+            // Using switch case to determine work hours
+            switch (employeeType) {
+                case 1: 
+                    workHours = fullDayHour; // Full-time employee
+                    System.out.println("Full-Time Employee");
+                    break;
+                case 0: 
+                    workHours = partTimeHour; // Part-time employee
+                    System.out.println("Part-Time Employee");
+                    break;
+                default: 
+                    workHours = 0; // Default case for unexpected values
+                    break;
             }
+
+            dailyWage = wagePerHour * workHours;
+            System.out.println("Employee Wage: " + dailyWage);
+
         } else {
             System.out.println("Employee is Absent");
         }
