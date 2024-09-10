6acb1aa575000bded86eae04f73575a9eb0bb01b
Joshua K John <joshuakjohn7@gmail.com>
2024-09-11 00:34:11 +0530

    added employee attendance feature to the code

--- EmpWageComputation.java
+++ EmpWageComputation.java
@@ -2,5 +2,14 @@ public class EmpWageComputation {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+        
+        // UC 1 - Check Employee is Present or Absent
+        int isPresent = (int)(Math.random() * 2); // Generates 0 or 1
+
+        if (isPresent == 1) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
